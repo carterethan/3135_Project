03ab8ca47eabfd3703a27fbe6b69f5ef819ba7e8
Ethan Carter <ethancarter64@gmail.com>
2019-11-11 20:07:05 -0500

    Assignment 3 - Added JS widgets and Jquery UI

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        slideWidth: 500,
+        pause: 3000,
+        pagerType: 'full',
+        wrapperClass: 'sectionImage'
+    });
+});
